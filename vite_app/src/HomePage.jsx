5ee98dc7c20c2076a73d132902ccfecfbe73dd53
Sujal Vasara <vasarasujal.cg@gmail.com>
2024-12-05 12:12:00 +0530

    added

--- vite_app/src/HomePage.jsx
+++ vite_app/src/HomePage.jsx
@@ -148,18 +148,22 @@ function HomePage() {
             </div>
 
             <div className="container5">
-        <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image11.png?raw=true"
-            alt="" className="images4"/>
-        <p className="new11">Dreaming On <br/>NEFEX</p>
-        <img className="new12"
-            src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/bx_bxs-heart.png?raw=true"
-            alt=""/>
-        <img className="images5"
-            src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Music%20Player%20Options.png?raw=true"
-            alt=""/>
-        <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Right%20options.png?raw=true"
-            alt="" className="images6"/>
-    </div>
+                <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image11.png?raw=true"
+                    alt="" className="images4" />
+                <p className="new11">Dreaming On <br />NEFEX</p>
+                <img className="new12"
+                    src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/bx_bxs-heart.png?raw=true"
+                    alt="" />
+                <img className="images5"
+                    src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Music%20Player%20Options.png?raw=true"
+                    alt="" />
+                <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Right%20options.png?raw=true"
+                    alt="" className="images6" />
+            </div>
+
+            <div className="new13">
+                <p className="new14">Listening on ASUS-ROG-G531GT</p>
+            </div>
 
 
 
